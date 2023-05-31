a07972b50d3d81124dcb7f3c9edbc77e52e83aa8
Brandon Ferrari <ferraribm@upmc.edu>
2023-05-31 09:11:43 -0400

    adding code to demo cancelNavigation bug

--- navbar/src/app/app.component.ts
+++ navbar/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'navbar-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "navbar-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
-export class AppComponent {
-  title = 'navbar';
+export class AppComponent implements OnInit {
+  title = "navbar";
+
+  ngOnInit(): void {
+    window.addEventListener("single-spa:before-routing-event", (event: any) => {
+      const detail = event.detail;
+
+      // console.log("***DETAIL***", detail);
+      if (detail.newUrl.includes("app1") && detail.oldUrl.includes("app2")) {
+        console.log("CANCELLED");
+        detail.cancelNavigation();
+      }
+    });
+  }
 }
